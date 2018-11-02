16de496effe75edde281fef359ec90a5305b0914
niko6116 <niko6116@github.com>
2018-11-02 10:05:19 +0100

    Batch Class created, DBSetup updated.

---
+++ MES/MES/data/Batch.cs
@@ -0,0 +1,76 @@
+﻿using MES.acq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MES.Data
+{
+    class Batch : IBatch
+    {
+
+        private readonly float batchId;
+        private readonly float beerId;
+        private readonly int acceptableProducts;
+        private readonly int defectProducts;
+        private readonly float temperature;
+        private readonly float humidity;
+        private readonly float vibration;
+        private readonly string timeStamp;
+
+        public Batch(float batchId, float beerId, int acceptableProducts,
+            int defectProducts, float temperature, float humidity,
+            float vibration, string timeStamp)
+        {
+            this.batchId = batchId;
+            this.beerId = beerId;
+            this.acceptableProducts = acceptableProducts;
+            this.defectProducts = defectProducts;
+            this.temperature = temperature;
+            this.humidity = humidity;
+            this.vibration = vibration;
+            this.timeStamp = timeStamp;
+        }
+
+        public float GetBatchId()
+        {
+            return batchId;
+        }
+
+        public float GetBeerId()
+        {
+            return beerId;
+        }
+
+        public int GetAcceptableProducts()
+        {
+            return acceptableProducts;
+        }
+
+        public int GetDefectProducts()
+        {
+            return defectProducts;
+        }
+
+        public float GetTemperature()
+        {
+            return temperature;
+        }
+
+        public float GetHumidity()
+        {
+            return humidity;
+        }
+
+        public float GetVibration()
+        {
+            return vibration;
+        }
+
+        public string GetTimestamp()
+        {
+            return timeStamp;
+        }
+    }
+}
--- MES/MES/data/DBSetup.cs
+++ MES/MES/data/DBSetup.cs
@@ -1,7 +1,9 @@
 ﻿using MES.acq;
 using MES.data;
+using MES.Data;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +13,7 @@ namespace MES
     class DBSetup
     {
         private IDBManager dbManager;
-        
+
         public DBSetup()
         {
             dbManager = new DBManager();
@@ -22,6 +24,14 @@ namespace MES
             bool cre = dbManager.CreateBatchesTable();
             // bool del = dbManager.DeleteBatchesTable();
             Console.WriteLine(cre);
+
+            IBatch batch = new Batch((float)1.2, (float)3.0, 90, 10,
+                (float)20.0, (float)10.5, (float)15.7, "02/11/2018 09:53:35");
+            bool inserted = dbManager.InsertIntoBatchesTable(batch);
+            Console.WriteLine(inserted);
+
+            DataTable table = dbManager.GetBatch((float)1.2);
+            Console.WriteLine(table.Rows.Contains((float)1.2));
         }
 
     }
