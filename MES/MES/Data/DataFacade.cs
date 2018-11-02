47f80dac710a89db5268249afa8518bf2bb56384
niko6116 <niko6116@github.com>
2018-11-02 11:37:29 +0100

    DataFacade

--- MES/MES/Data/DataFacade.cs
+++ MES/MES/Data/DataFacade.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MES.acq;
+using MES.data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,29 @@ using System.Threading.Tasks;
 
 namespace MES.Data
 {
-    class DataFacade
+    class DataFacade : IData
     {
+
+        IDBManager dbManager;
+
+        public DataFacade()
+        {
+            dbManager = new DBManager();
+        }
+
+        public bool SaveBatch(float batchId, float beerId, int acceptableProducts,
+            int defectProducts, float temperature, float humidity,
+            float vibration, string timeStamp)
+        {
+            return dbManager.InsertIntoBatchesTable(
+                new Batch(batchId, beerId, acceptableProducts,
+                defectProducts, temperature, humidity,
+                vibration, timeStamp));
+        }
+
+        public bool SaveBatch(IBatch batch)
+        {
+            return dbManager.InsertIntoBatchesTable(batch);
+        }
     }
 }
---
+++ MES/MES/acq/IData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MES.acq
+{
+    interface IData
+    {
+
+        bool SaveBatch(float batchId, float beerId, int acceptableProducts,
+            int defectProducts, float temperature, float humidity,
+            float vibration, string timeStamp);
+
+        bool SaveBatch(IBatch batch);
+
+    }
+}
