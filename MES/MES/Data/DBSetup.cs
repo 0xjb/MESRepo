881ed0585be4f3803e72f873946bd0260bae9a01
Brian <brianchr4@gmail.com>
2018-11-07 10:36:53 +0100

    linjer

--- MES/MES/Data/DBSetup.cs
+++ MES/MES/Data/DBSetup.cs
@@ -37,6 +37,7 @@ namespace MES
             //{
             //    Console.WriteLine(element.ToString());
             //}
+
             //Console.WriteLine(dbManager.GetBatch(7).ToString());
             //DataTable table = dbManager.GetBatch((float)1.2);
             //Console.WriteLine(table.Rows.Contains((float)1.2));
