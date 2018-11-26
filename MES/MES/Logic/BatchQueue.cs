4490ee695eaf36f29d69907ea01de706ec7e9f29
0xjb <Jonas_46@msn.com>
2018-11-26 10:14:15 +0100

    lmao

--- MES/MES/Logic/BatchQueue.cs
+++ MES/MES/Logic/BatchQueue.cs
@@ -58,10 +58,15 @@ namespace MES.Logic {
         private void CheckBatchProdStatus(object sender, PropertyChangedEventArgs e) {
            if(e.PropertyName.Equals("StateCurrent")) {
                 if((sender as OpcClient).StateCurrent == 17) {
-                    if(Batches[0] != null) {
+                    //TODO: Fix this
+                    try {
                         CurrentBatch = Batches[0];
                         Batches.RemoveAt(0);
+                    } catch(ArgumentOutOfRangeException ex) {
+                        Console.WriteLine(":)");
                     }
+                        
+                    
 
                 }
             }
